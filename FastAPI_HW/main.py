8be0e130996546f6ec3d75f06d393bad8bb277d9
Neha-Chandrika <samvida1986@gmail.com>
2026-07-20 22:08:59 +0530

    FastAPI first class

---
+++ FastAPI_HW/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+#End point
+@app.get("/")
+def read_root():
+    return {"message": "Hello, FastAPI!"}
+
+@app.get("/greet")
+def greet_user(name:str):
+    return {"message": "Hello, " + name + "!"}
