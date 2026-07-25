470eb02a6197d734d62b0d5139f05458501a611c
Neha-Chandrika <samvida1986@gmail.com>
2026-07-25 22:18:01 +0530

    add immediately invoked function for exam settings

---
+++ functions/immediatlyInvokeFunctions.js
@@ -0,0 +1,8 @@
+(function (){
+    let examDuration = 60;
+    let totalQuestions = 50;
+
+    console.log("Exam settings loaded");
+    console.log("Duration:", examDuration, "minutes");
+    console.log("Questions:", totalQuestions);
+})();
